5cb0d5c5738a60e85534777aab1fdbfa97652b22
hehehahahoobausa <bausa.gb@obmontessori.edu.ph>
2026-09-03 19:03:48 +0800

    new

---
+++ main.py
@@ -0,0 +1,27 @@
+from pyscript import display
+
+display("Determine the types below", target="div1") #literal object
+
+#variable object
+
+name = "Bausa G" # this is astring
+age = 15 #this is now an integer
+height2026 = 162.56 #now we move on to a float
+countries_now = ["Russia", "Malaysia", "Canada"] #this data type is a list
+student_type = False #this is a boolean, i am a veteran obmc student
+dictionary = {
+         "color": "red",
+         "car_brand": "Cadillac",
+         "shoe_size": "8.5",
+         "best_friend": 
+} #this is a dictionary, many things here
+
+my_favfruits = 
+
+display(type(name), target='div1')
+display(type(age), target='div1')
+display(type(height), target='div1')
+display(type(is_student), target='div1')
+display(type(movies), target='div1')
+display(type(hobbies), target='div1')
+display(type(g), target='div1')
